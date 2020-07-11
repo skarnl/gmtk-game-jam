04fef5365feea2c373dcc4c58dd474bd772f5022
Oskar van Velden <oskar@rakso.nl>
2020-07-11 15:22:57 +0200

    Simplified player movement

--- entities/player.gd
+++ entities/player.gd
@@ -83,10 +83,10 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 		
 	if input_vector != Vector2.ZERO:
-		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+		velocity = input_vector * MAX_SPEED
 		
 	else:
-		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		velocity = Vector2.ZERO
 	
 	move_and_slide(velocity)
 	_update_shooting_indicator()
