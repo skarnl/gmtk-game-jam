9c33fb8f64c3c7409dd79cace8a75d358d74d7cc
Oskar van Velden <oskar@rakso.nl>
2020-07-11 14:55:41 +0200

    Set enemy movement speed with a little randomness

--- entities/enemy.gd
+++ entities/enemy.gd
@@ -3,23 +3,30 @@ extends KinematicBody2D
 signal player_killed
 signal enemy_killed
 
-const WALKING_SPEED = 100 #RANDOMIZE
+const BASE_WALKING_SPEED = 100 #RANDOMIZE
 
-var points_when_killed = 3
+var points_when_killed = 1
 var player_reference
 
+var movement_speed
+
 func _ready():
+	var rnd = RandomNumberGenerator.new()
+	rnd.randomize()
+	
 	add_to_group('spawned')
 	
 	var nodes = get_tree().get_nodes_in_group('player')
 	player_reference = nodes.front()
+	
+	movement_speed = BASE_WALKING_SPEED + rnd.randf_range(-30, 60)
 
 func _physics_process(delta):
 	if not player_reference:
 		return
 		
 	var motion = (player_reference.get_global_position() - global_position)
-	move_and_slide(motion.normalized() * WALKING_SPEED)
+	move_and_slide(motion.normalized() * movement_speed)
 	look_at(player_reference.position)
 
 
