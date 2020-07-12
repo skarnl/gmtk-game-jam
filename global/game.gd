e09b18dadd887b5400592d2b6bf113bd3a501bc2
Oskar van Velden <oskar@rakso.nl>
2020-07-12 15:32:36 +0200

    Add main menu and tweak intro sequence

--- global/game.gd
+++ global/game.gd
@@ -33,67 +33,13 @@ func start_game():
 func transition_to(new_state: int) -> void:
 	match new_state:
 		GameState.MAIN_MENU:
-			transition_to(GameState.GAME)
-#			_current_state = new_state
-#			SceneLoader.goto_scene(Screens.MAIN_MENU)
+			_current_state = new_state
+			SceneLoader.goto_scene(Screens.MAIN_MENU)
 		
 		GameState.GAME:
-			if _current_state in {GameState.SPLASH: true}:
+			if _current_state in {GameState.SPLASH: true, GameState.MAIN_MENU: true}:
 				_current_state = new_state
 				SceneLoader.goto_scene(Screens.GAME)
-				
-#			match new_state:
-#				GameState.PAUSED:
-#					_current_state = GameState.PAUSED
-#					emit_signal("game_paused")
-#					get_tree().paused = true
-#
-#				GameState.GAME_DIALOG_OPENED:
-#					_current_state = GameState.GAME_DIALOG_OPENED
-#					get_tree().paused = true
-#
-#				GameState.GAME_OVER:
-#					_current_state = GameState.GAME_OVER
-#					get_tree().paused = true
-#					emit_signal("game_over")
-#
-#		GameState.GAME_DIALOG_OPENED:
-#				match new_state:
-#					GameState.GAME:
-#						_current_state = GameState.GAME
-#						get_tree().paused = false
-#
-#						if is_current_level_finished:
-#							emit_signal("level_finished")
-#							get_tree().paused = true
-#
-#					GameState.PAUSED:
-#						_current_state = GameState.PAUSED
-#						emit_signal("game_paused")
-#						get_tree().paused = true
-#
-#		GameState.GAME_OVER:
-#			if new_state == GameState.MENU:
-#				_current_state = GameState.MENU
-#				get_tree().paused = false
-#				SceneLoader.goto_scene('res://menu/main_menu.tscn')
-#
-#		GameState.PAUSED:
-#			match new_state:
-#				GameState.MENU:
-#					_current_state = GameState.MENU
-#					get_tree().paused = false
-#					emit_signal("game_resumed")
-#					SceneLoader.goto_scene('res://menu/main_menu.tscn')
-#
-#				GameState.GAME:
-#					_current_state = GameState.GAME
-#					get_tree().paused = false
-#					emit_signal("game_resumed")
-#
-#				GameState.GAME_DIALOG_OPENED:
-#					_current_state = GameState.GAME_DIALOG_OPENED
-#					emit_signal("game_resumed")
 
 
 # pause handler
--- screens/main_menu.gd
+++ screens/main_menu.gd
@@ -1,4 +1,12 @@
-extends Control
+extends Node2D
 
 func _ready():
-	Game.start_game()
+	pass
+
+func _input(event):
+	if event is InputEventMouseButton:
+		$StartGamePlayer.play('start_game')
+		
+		yield($StartGamePlayer, 'animation_finished')
+		
+		Game.start_game()
