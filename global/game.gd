6ac6ac8cbaf5305fd2575da7e028e766f2b5da32
Oskar van Velden <oskar@rakso.nl>
2020-09-24 22:07:34 +0200

    Simplify state management

--- global/game.gd
+++ global/game.gd
@@ -5,41 +5,55 @@ extends Node
 
 var Screens = {
 	MAIN_MENU = 'res://screens/main_menu.tscn',
-	GAME = 'res://screens/game/game.tscn',
+	GAME = 'res://screens/game.tscn',
 }
 
 
-enum GameState {
+enum {
 	SPLASH,
 	MAIN_MENU,
+	START_GAME,
 	GAME,
 	PAUSED,
 	GAME_OVER
 }
 
-var _current_state: int = GameState.SPLASH setget _set_current_state
-var _previous_state: int
-
+var _current_state: int = SPLASH
+var _valid_state_changes = {
+	SPLASH: [MAIN_MENU],
+	MAIN_MENU: [START_GAME],
+	START_GAME: [GAME],
+	GAME: [PAUSED, GAME_OVER],
+	PAUSED: [GAME, MAIN_MENU],
+	GAME_OVER: [MAIN_MENU]
+}
 
 func _ready():
 	pause_mode = Node.PAUSE_MODE_PROCESS
 	
 	
 func start_game():
-	transition_to(GameState.GAME)
+	transition_to(START_GAME)
 	
+
+func is_transition_valid(next_state: int) -> bool:
+	return next_state in _valid_state_changes[_current_state]
+
 
 # change the state to the next
 func transition_to(new_state: int) -> void:
+	if !is_transition_valid(new_state):
+		return
+	
+	_current_state = new_state
+	
 	match new_state:
-		GameState.MAIN_MENU:
-			_current_state = new_state
+		MAIN_MENU:
 			SceneLoader.goto_scene(Screens.MAIN_MENU)
 		
-		GameState.GAME:
-			if _current_state in {GameState.SPLASH: true, GameState.MAIN_MENU: true}:
-				_current_state = new_state
-				SceneLoader.goto_scene(Screens.GAME)
+		START_GAME:
+			SceneLoader.goto_scene(Screens.GAME)
+			transition_to(GAME)
 
 
 # pause handler
@@ -47,13 +61,8 @@ func _input(event):
 	if event is InputEventKey:
 		if event.is_action_pressed('ui_pause'):
 			match _current_state:
-				GameState.GAME:
-					transition_to(GameState.PAUSED)
+				GAME:
+					transition_to(PAUSED)
 			
-				GameState.PAUSED:
-					transition_to(GameState.GAME)
-
-
-func _set_current_state(new_state:int) -> void:
-	_previous_state = _current_state
-	_current_state = new_state
+				PAUSED:
+					transition_to(GAME)
