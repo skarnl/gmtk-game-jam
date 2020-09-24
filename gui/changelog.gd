d66d06d7ef99e61cbc3df490842060b9cc222b95
Oskar van Velden <oskar@rakso.nl>
2020-09-24 22:08:08 +0200

    Hide effect / debug viewing

--- gui/changelog.gd
+++ gui/changelog.gd
@@ -1,7 +1,8 @@
 extends Control
 
 func _ready():
-	RandomEffectController.connect('effect_applied', self, '_on_REC_effect_applied')
+	hide()
+#	RandomEffectController.connect('effect_applied', self, '_on_REC_effect_applied')
 	
 func _on_REC_effect_applied(logEntry):
 	$Logs.text += '%s\n' % logEntry
